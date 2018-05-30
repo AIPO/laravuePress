6edfd368b54fafaf8e353cae6e4c55e1160d942d
AIPO <p.vaitonis@gmail.com>
2018-05-30 16:43:42 +0300

    Created Post model and controller on filled migration

---
+++ database/migrations/2018_05_30_130102_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'posts', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('slug')->unique(); //to reach post by slug istead of $idea
+                $table->integer('author_id'); //for displaying author near post
+                $table->string('title'); //title
+                $table->text('excerpt'); //summary of content
+                $table->longText('content'); // full content
+                $table->enum('status', ['published', 'draft', 'deleted']); // post statuses in admin
+                $table->integer('comment_count')->unsigned();
+                $table->dateTime('published_at');
+                $table->timestamps();
+                $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
