9b9c66f9a027b3072f858f65a1ec569a96784227
AIPO <p.vaitonis@gmail.com>
2018-06-22 15:10:25 +0300

    fresh auth for setup without packages.

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laratrust\Traits\LaratrustUserTrait;
 
 /**
  * App\Models\User
@@ -31,7 +30,6 @@ use Laratrust\Traits\LaratrustUserTrait;
  */
 class User extends Authenticatable
 {
-    use LaratrustUserTrait;
     use HasApiTokens, Notifiable;
 
     /**
